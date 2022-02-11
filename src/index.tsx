8bfa19e25b7ec0d340cfdec12b331f23abce062c
jiyong LEE <49055628+Jiyong95@users.noreply.github.com>
2022-02-11 14:17:26 +0900

    :sparkles: redux-persit 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from 'store';
+import { PersistGate } from 'redux-persist/integration/react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const { store, persistor } = configureStore();
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+
+  document.getElementById('root')
+);
