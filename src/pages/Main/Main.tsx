947b8082236025d9e850fe09f40ed8a9b4569a45
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-02-12 05:46:35 +0900

    :bug: MessageCard에 key값을 제대로 바인딩함

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,49 +1,51 @@
-import "./Main.scss";
-import { useSelector } from "react-redux";
-import { StoreState } from "types/store";
-import MessageCard from "components/messageCard/messageCard";
+import './Main.scss';
+import { useSelector } from 'react-redux';
+import { StoreState } from 'types/store';
+import MessageCard from 'components/messageCard/messageCard';
 import Login from 'components/Login/Login';
 import { useState } from 'react';
 
 const Main = () => {
   const messageState = useSelector((state: StoreState) => state.message);
   const [LoginModal, setLoginModal] = useState<boolean>(true);
-  const userState = useSelector((state:StoreState)=> state.auth);
+  const userState = useSelector((state: StoreState) => state.auth);
   if (LoginModal) {
-    document.body.style.overflow = "hidden";
+    document.body.style.overflow = 'hidden';
   }
 
   return (
     <>
-    {LoginModal && !userState.userName && <Login setLoginModal={setLoginModal} />}
-    <div className="main-container">
-      <div className="header">
-        <div className="home-wrap">
-          <div className="home-icon"></div>
+      {LoginModal && !userState.userName && (
+        <Login setLoginModal={setLoginModal} />
+      )}
+      <div className="main-container">
+        <div className="header">
+          <div className="home-wrap">
+            <div className="home-icon"></div>
+          </div>
+          <div className="header-title">Swit</div>
         </div>
-        <div className="header-title">Swit</div>
-      </div>
-      <div className="main">
-        <div className="sidebar">
-          <div className="move-to-chat"></div>
-        </div>
-        <div className="section">
-          <div className="nav">💛General</div>
-          <div className="chat-background">
-            <div className="date-line">
-              <div className="line"></div>
-              <span className="date">Thursday, August 22, 2019</span>
-              <div className="line"></div>
-            </div>
-            <div className="chat-section">
-              {messageState.map((data) => (
-                <MessageCard key={data.userId} msg={data} />
-              ))}
+        <div className="main">
+          <div className="sidebar">
+            <div className="move-to-chat"></div>
+          </div>
+          <div className="section">
+            <div className="nav">💛General</div>
+            <div className="chat-background">
+              <div className="date-line">
+                <div className="line"></div>
+                <span className="date">Thursday, August 22, 2019</span>
+                <div className="line"></div>
+              </div>
+              <div className="chat-section">
+                {messageState.map((data) => (
+                  <MessageCard key={data.date} msg={data} />
+                ))}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
