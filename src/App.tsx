c52069dc356bd8852c8567bd0b2bb01f2753ec15
Yena-Yun <cheryleduart@gmail.com>
2022-02-11 16:00:39 +0900

    :fire: jsconfig.json 파일 삭제, App과 index에 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Main from 'pages/Main/Main';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path='/main' element={<Main />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import { Provider } from 'react-redux';
 import configureStore from 'store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
 
 const { store, persistor } = configureStore();
+
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
