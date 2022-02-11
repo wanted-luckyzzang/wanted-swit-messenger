6ccc96ec574056c54cce0fcc828a349dbc7310fb
Yena-Yun <cheryleduart@gmail.com>
2022-02-11 20:39:08 +0900

    :bug: messageCard의 scss 적용 안 되던 부분 수정

--- src/components/messageCard/messageCard.tsx
+++ src/components/messageCard/messageCard.tsx
@@ -1,25 +1,21 @@
-import MessageModal from "components/messageModal/messageModal";
-import { MessageData } from "types/store";
-import "./messageCard.scss";
+import MessageModal from 'components/messageModal/messageModal';
+import { MessageData } from 'types/store';
+import './messageCard.scss';
 
 const MessageCard = (props: { msg: MessageData }): JSX.Element => {
   return (
-    <div className="message-card-container">
+    <div className='message-card-container'>
       <MessageModal />
-      <div className="messageCard">
-        <div className="user-profile">
-          <img
-            className="image"
-            src={props.msg.profileImage}
-            alt="profileImage"
-          />
+      <div className='message-card'>
+        <div className='user-profile'>
+          <img className='image' src={props.msg.profileImage} alt='profileImage' />
         </div>
-        <div className="content">
-          <span className="name-date">
-            <span className="name">{props.msg.userName}</span>
-            <span className="date">{props.msg.date}</span>
+        <div className='content'>
+          <span className='name-date'>
+            <span className='name'>{props.msg.userName}</span>
+            <span className='date'>{props.msg.date}</span>
           </span>
-          <div className="message">{props.msg.content}</div>
+          <div className='message'>{props.msg.content}</div>
         </div>
       </div>
     </div>
