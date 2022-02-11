e4b5b4d7761d49410f3ab129e9760266929184e5
minjuice1 <minjuice93@gmail.com>
2022-02-12 00:23:04 +0900

    :sparkles: 로그인페이지 이미지 업로드 기능 추가

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,22 +1,35 @@
-import ImgPreviewChange from 'hooks/ImageUpload';
-import { useEffect, useRef, useState} from 'react';
+import { useRef, useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from "react-router-dom";
 import { login } from 'store/actions';
 import { StoreState } from 'types/store';
-import "../SignUp/SignUp";
+import "../Login/Login.scss";
 
 const Login = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const userState = useSelector((state:StoreState)=>state.auth);
   const inputEl = useRef<HTMLInputElement | null>(null);
+  const [profileImage, setProfileImg] = useState<string>();
+
+  const imageHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if(reader.readyState === 2){
+        const url = reader.result as string;
+        setProfileImg(url)
+      }
+    }
+    if(e.target.files)
+      reader.readAsDataURL(e.target.files[0])
+  };
 
   const submitHandler = () => {
     const {current} = inputEl;
     const userName = current?.value;
-    dispatch(login({userName}))
-    // navigate("/main")
+    if(profileImage)
+      dispatch(login({userName, profileImage}))
+    navigate("/main")
   }
 
   return(
@@ -30,6 +43,18 @@ const Login = () => {
           <h4>아이디</h4>
           <input ref={inputEl} name="username" type="text" placeholder='아이디를 입력하세요' required/>
         </div>
+        <div className="signtup-profileEdit">
+					  <h1>프로필 이미지</h1>
+            <div className="signtup-profileEdit-imgBox">
+              {profileImage && <img src={profileImage} alt="" id="img" className="img" />}
+            </div>
+            <input type="file" accept="image/*" name="image-upload" id="input" onChange={imageHandler} />
+            <div className="label">
+              <label htmlFor="input">
+                이미지를 등록해주세요.
+              </label>
+            </div>
+				  </div>
         <div className='signup-buttonBox'>
           <button className='signup-button' onClick={submitHandler}>로그인</button>
         </div>
