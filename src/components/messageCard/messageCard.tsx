f4225d9597208521cecd9fe2cabe3cf35d85a057
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-02-11 21:50:05 +0900

    :sparkles: ProfilePhoto 컴포넌트 추가

--- src/components/messageCard/messageCard.tsx
+++ src/components/messageCard/messageCard.tsx
@@ -1,21 +1,22 @@
 import MessageModal from 'components/messageModal/messageModal';
 import { MessageData } from 'types/store';
 import './messageCard.scss';
+import ProfilePhoto from './profilePhoto';
 
 const MessageCard = (props: { msg: MessageData }): JSX.Element => {
   return (
-    <div className='message-card-container'>
+    <div className="message-card-container">
       <MessageModal />
-      <div className='message-card'>
-        <div className='user-profile'>
-          <img className='image' src={props.msg.profileImage} alt='profileImage' />
+      <div className="message-card">
+        <div className="user-profile">
+          <ProfilePhoto image={props.msg.profileImage} />
         </div>
-        <div className='content'>
-          <span className='name-date'>
-            <span className='name'>{props.msg.userName}</span>
-            <span className='date'>{props.msg.date}</span>
+        <div className="content">
+          <span className="name-date">
+            <span className="name">{props.msg.userName}</span>
+            <span className="date">{props.msg.date}</span>
           </span>
-          <div className='message'>{props.msg.content}</div>
+          <div className="message">{props.msg.content}</div>
         </div>
       </div>
     </div>
