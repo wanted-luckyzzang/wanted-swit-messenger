d953e5a46f94a4d1f6b325f29072edb7a16a920c
Yena-Yun <cheryleduart@gmail.com>
2022-02-12 04:06:47 +0900

    :sparkles: textarea 메시지 작성, 전송버튼 클릭 시 렌더링

--- src/components/messageInput/messageInput.tsx
+++ src/components/messageInput/messageInput.tsx
@@ -9,28 +9,38 @@ const MessageInput = () => {
   const [input, setInput] = useState<string>('');
   // const userState = useSelector((state: StoreState) => state.auth);
 
-  const handleOnSubmit = (e: React.MouseEvent) => {
-    e.preventDefault();
+  const handleOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(e.target.value);
+  };
+
+  const handleOnSubmit = () => {
     const formatedDate = formatDate();
 
     dispatch(sendMessage({ content: input, date: formatedDate }));
     setInput('');
   };
 
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    setInput(value);
+  const onKeyHandler = (e: React.KeyboardEvent<HTMLButtonElement>) => {
+    if (input && e.key === 'Enter') {
+      e.preventDefault();
+      handleOnSubmit();
+    }
   };
 
   return (
     <div className='input-container'>
-      <div className='input-box'>
-        <div className='input-wrap'>
-          <input name='message' type='text' value={input} placeholder='메시지를 입력하세요..' onChange={handleOnChange} />
-          <button className='send-btn' onClick={handleOnSubmit}>
-            전송
-          </button>
-        </div>
+      <div className='input-wrap'>
+        <textarea
+          className='message-input'
+          name='message-input'
+          value={input}
+          cols={180}
+          placeholder='메시지를 입력하세요..'
+          onChange={handleOnChange}
+        />
+        <button className='send-btn' onClick={handleOnSubmit} onKeyPress={onKeyHandler}>
+          전송
+        </button>
       </div>
     </div>
   );
