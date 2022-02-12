c8c7c63929171c7b2183e9db9b3eba03afe6dea9
jiyong LEE <49055628+Jiyong95@users.noreply.github.com>
2022-02-12 11:52:28 +0900

    :bug: 메세지 줄바꿈 안되던 문제 해결

--- src/components/messageInput/messageInput.tsx
+++ src/components/messageInput/messageInput.tsx
@@ -79,27 +79,28 @@ const MessageInput = () => {
   return (
     <>
       {userState.userName && (
-        <div className='input-wrapper'>
-        <div className="input-container">
-          <div className="input-wrap">
-            <textarea
-              ref={textEl}
-              className="message-input"
-              name="message-input"
-              value={input}
-              cols={180}
-              onKeyDown={textAreaHandler}
-              placeholder="메시지를 입력하세요.."
-              onChange={handleOnChange}
-              autoFocus
-            />
-            <SendButton
-              onSubmit={handleOnSubmit}
-              keyHandler={onKeyHandler}
-              active={active}
-            />
+        <div className="input-wrapper">
+          <div className="input-container">
+            <div className="input-wrap">
+              <textarea
+                ref={textEl}
+                className="message-input"
+                name="message-input"
+                value={input}
+                cols={180}
+                onKeyDown={textAreaHandler}
+                placeholder="메시지를 입력하세요.."
+                onChange={handleOnChange}
+                wrap="hard"
+                autoFocus
+              />
+              <SendButton
+                onSubmit={handleOnSubmit}
+                keyHandler={onKeyHandler}
+                active={active}
+              />
+            </div>
           </div>
-        </div>
         </div>
       )}
     </>
