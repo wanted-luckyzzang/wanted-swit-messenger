4828a8e9f9ab33a482ada097cc71224f35ef3d89
minjuice1 <minjuice93@gmail.com>
2022-02-12 10:25:46 +0900

    :sparkles: 새로 단 댓글로 scroll 따라다니기 기능 추가

--- src/components/messageCard/messageCard.tsx
+++ src/components/messageCard/messageCard.tsx
@@ -1,13 +1,27 @@
 import MessageModal from 'components/messageModal/messageModal';
+import { useCallback, useEffect, useRef } from 'react';
 import { MessageData } from 'types/store';
 import './messageCard.scss';
 import ProfilePhoto from './profilePhoto';
 
-const MessageCard = (props: { msg: MessageData }): JSX.Element => {
+const MessageCard = (props: { msg: MessageData }, messageState): JSX.Element => {
+  const scrollEl = useRef<HTMLDivElement>(null);
+  
+  const scrollToBottom = useCallback(() => {
+    if (messageState) {
+      scrollEl.current?.scrollIntoView({behavior: 'smooth', block: 'nearest', inline: 'nearest'});
+    }
+  }, [messageState]);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [scrollToBottom])
+
+
   return (
-    <div className='message-card-container'>
+    <div className='message-card-container'ref={scrollEl}>
       <MessageModal data={props.msg} />
-      <div className='message-card'>
+      <div className='message-card' >
         <div className='user-profile'>
           <ProfilePhoto image={props.msg.profileImage} />
         </div>
