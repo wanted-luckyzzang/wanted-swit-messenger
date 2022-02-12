0310e704568097b5937af8702596010baead2923
jiyong LEE <49055628+Jiyong95@users.noreply.github.com>
2022-02-12 10:53:11 +0900

    :sparkles: 메세지 입력창 마지막으로 스크롤 이동 추가

--- src/components/messageInput/messageInput.tsx
+++ src/components/messageInput/messageInput.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './messageInput.scss';
 import { formatDate } from 'utils/formatDate';
@@ -12,6 +12,7 @@ const MessageInput = () => {
   const [active, setActive] = useState('nonactive');
   const userState = useSelector((state: StoreState) => state.auth);
   const answerState = useSelector((state: StoreState) => state.answer);
+  const scrollEl = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (answerState.content) {
@@ -21,6 +22,20 @@ const MessageInput = () => {
       setInput('');
     }
   }, [answerState]);
+
+  const scrollToBottom = useCallback(() => {
+    if (input) {
+      scrollEl.current?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'nearest',
+      });
+    }
+  }, [input]);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [scrollToBottom]);
 
   const handleOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInput(e.target.value);
@@ -61,7 +76,7 @@ const MessageInput = () => {
   return (
     <>
       {userState.userName && (
-        <div className="input-container">
+        <div className="input-container" ref={scrollEl}>
           <div className="input-wrap">
             <textarea
               className="message-input"
