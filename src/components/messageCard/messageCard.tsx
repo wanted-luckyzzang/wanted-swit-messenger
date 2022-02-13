663936eea6dbf24a63b03d5e1346f9e8d383390c
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-02-13 12:23:20 +0900

    :recycle: messageCard에 쓰이는 불필요한 함수들 제거

--- src/components/messageCard/messageCard.tsx
+++ src/components/messageCard/messageCard.tsx
@@ -1,30 +1,12 @@
-import React, { useCallback, useEffect, useRef } from 'react';
-import { useSelector } from 'react-redux';
-import { MessageData, StoreState } from 'types/store';
+import React from 'react';
+import { MessageData } from 'types/store';
 import MessageModal from 'components/messageModal/messageModal';
 import ProfilePhoto from './profilePhoto';
 import './messageCard.scss';
 
 const MessageCard = (props: { msg: MessageData }): JSX.Element => {
-  const scrollEl = useRef<HTMLDivElement>(null);
-  const messageState = useSelector((state: StoreState) => state.message);
-
-  const scrollToBottom = useCallback(() => {
-    if (messageState) {
-      scrollEl.current?.scrollIntoView({
-        behavior: 'smooth',
-        block: 'nearest',
-        inline: 'nearest',
-      });
-    }
-  }, [messageState]);
-
-  useEffect(() => {
-    scrollToBottom();
-  }, [scrollToBottom]);
-
   return (
-    <div className="message-card-container" ref={scrollEl}>
+    <div className="message-card-container">
       <MessageModal data={props.msg} />
       <div className="message-card">
         <div className="user-profile">
